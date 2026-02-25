9a6f78e4f35b12d0f79bdf7e6d4fad5f1e610b1e
MountainBird890 <206936394+MountainBird890@users.noreply.github.com>
2026-02-25 09:27:25 +0900

    3.リストページの行削除、行追加ボタンの実装（デバック未実施）

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import SideBar from './setSideBar';
 import SetTable from './setTable';
+import { Button } from 'antd';
+import { useState } from 'react';
+import useData from './sampleData';
+
+
 
 function App(){
+    const [data, setRow] = useState(useData());
+
+      const newData = {
+        key: '',
+        name: '',
+        office: '',
+        holiday: '',
+        pto: '',
+        yamada: '',
+        suzuki: '',
+      };
+
+      const AddRow =()=> {
+        console.log(data);
+        const lastKey = data.length > 0 ? data[data.length - 1].key : '0';
+        console.log(lastKey);
+        const newKey = String(Number(lastKey) + 1);
+        console.log(newKey);
+        setRow([{ ...newData, key: newKey }, ...data]);
+      };
 
 return(
      <div className="layout">
         <SideBar />
     <div className="list">
+        <Button onClick={AddRow}>行を追加</Button>
         <SetTable />
     </div>
     </div>
--- src/setTable.tsx
+++ src/setTable.tsx
@@ -1,8 +1,10 @@
-import { Table } from 'antd';
+import React, { useState } from 'react';
+import { Table, Button } from 'antd';
 import type { TableColumnsType } from 'antd';
+import useData from './sampleData';
 
 interface DataType {
-  key: string;
+  key: React.Key;
   name: string;
   office: string;
   holiday: string;
@@ -13,144 +15,33 @@ interface DataType {
 
 export default function SetTable(){
 
+  const [data, setData] = useState(useData());
+
+  const deleteRow = (key: React.Key) =>{
+      setData(data.filter(item => item.key !== key));
+    }
+  
+
     const columns: TableColumnsType<DataType> = [
-  { title: '氏名', dataIndex: 'name' },
-  { title: '内勤', dataIndex: 'office' },
-  { title: '公休', dataIndex: 'holiday' },
-  { title: '有給', dataIndex: 'pto' },
-  { title: '山田', dataIndex: 'yamada' },
-  { title: '鈴木', dataIndex: 'suzuki' },
-    ];
+    {
+      title: '操作',
+      dataIndex: 'control',
+      render: (_, record) =>(
+        <>
+        
+        <Button danger onClick={()=>deleteRow(record.key)}>削除</Button>
+        </>
+      )
+    },
+    { title: '氏名', dataIndex: 'name' },
+    { title: '内勤', dataIndex: 'office' },
+    { title: '公休', dataIndex: 'holiday' },
+    { title: '有給', dataIndex: 'pto' },
+    { title: '山田', dataIndex: 'yamada' },
+    { title: '鈴木', dataIndex: 'suzuki' },
+  ];
 
-    const data = [
-  {
-    key: '1',
-    name: '兵庫太郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '2',
-    name: '兵庫次郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '3',
-    name: '兵庫三郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '4',
-    name: '兵庫四郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '5',
-    name: '兵庫五郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '6',
-    name: '兵庫権六',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '7',
-    name: '兵庫七郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '8',
-    name: '兵庫権八',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '9',
-    name: '兵庫九郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '10',
-    name: '兵庫十兵衛',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '11',
-    name: '摂津一朗太',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '12',
-    name: '摂津次郎右衛門',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '13',
-    name: '摂津三太夫',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-  {
-    key: '14',
-    name: '摂津幸四郎',
-    office: '○',
-    holiday: '',
-    pto: '○',
-    yamada: '○',
-    suzuki: '',
-  },
-
-    ];
-
-    return <Table columns={columns} dataSource={data}/>
+    return (
+    <Table columns={columns} dataSource={data}/>
+  );
 };
