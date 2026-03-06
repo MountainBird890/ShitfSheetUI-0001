c16b4249fd622ab09adc8652b6079442d96599de
MountainBird890 <206936394+MountainBird890@users.noreply.github.com>
2026-03-06 11:40:48 +0900

    13.シフトテーブル追加

--- src/renderer/features/shift/pages/layout.tsx
+++ src/renderer/features/shift/pages/layout.tsx
@@ -1,6 +1,7 @@
 import SideBar from "../pages/setSideBar";
 import { Button } from "antd";
 import { setAdd } from "../ui/addRow";
+import SetTable from "./setTable";
 
 export default function Shift(){
 
@@ -9,7 +10,7 @@ export default function Shift(){
         <SideBar />
     <div className="list">
         <Button onClick={setAdd}>行を追加</Button>
-
+        <SetTable />
     </div>
     </div>
   )
