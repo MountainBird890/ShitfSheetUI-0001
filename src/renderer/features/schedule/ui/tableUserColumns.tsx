153ac58dea7c53df60df60d163506d37760747ff
MountainBird890 <206936394+MountainBird890@users.noreply.github.com>
2026-04-27 15:44:42 +0900

    49.package.jsonのwinをパッケージインストール用に修正。nsisを追加

--- src/renderer/features/schedule/ui/tableUserColumns.tsx
+++ src/renderer/features/schedule/ui/tableUserColumns.tsx
@@ -62,6 +62,7 @@ export default function UserColumn(value: Dayjs) {
     <Table<any>
       columns={weekColumns}
       dataSource={data.basedata}
+      rowKey="staffId"
       bordered
       size="middle"
       scroll={{ x: 'max-content', y: 'max-content'}}
