d3000ee024e26a8150fa511e8a7fc9e1659fcf8f
MountainBird890 <206936394+MountainBird890@users.noreply.github.com>
2026-04-28 10:23:23 +0900

    68.main.tsのwin.loadFIleを修正

--- electron/main.ts
+++ electron/main.ts
@@ -47,7 +47,7 @@ function createWindow() {
     win.loadURL("http://localhost:5173");
     win.webContents.openDevTools();
   } else {
-    win.loadFile(path.join(__dirname, "../dist/index.html"));
+    win.loadFile(path.join(process.resourcesPath, "app.asar/dist/index.html"));
   }
 }
 
