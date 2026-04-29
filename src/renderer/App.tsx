18a578c6e37e55abb4d974027de82138a095298c
MountainBird890 <206936394+MountainBird890@users.noreply.github.com>
2026-04-29 13:41:49 +0900

    86..src/renderer/App.tsxをHashRouterで囲むように修正

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,23 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import SideBar from "./features/setSideBar";
 import Shift from "./features/list/pages/layout";
 import Schedule from "./features/schedule/pages/SchedulePage";
 import KimuKeitai from "./features/Kinmu/pages/kinmupage";
 import Calendar from "./features/calendar/pages/calendarpage";
 import { Sheet } from "./features/shift/pages/shiftpage";
-import { HashRouter as Routes, Route } from 'react-router-dom';
 
 export default function App(){
-    return(
-        <Routes>
-            <Route path="/" element={<SideBar />}>
-            <Route path="/shift" element={<Shift />}/>
-            <Route path="/schedule" element={<Schedule />}/>
-            <Route path="/calendar" element={<Calendar />}/>
-            <Route path="/kinmu" element={<KimuKeitai />}/>
-            <Route path="/sheet" element={<Sheet />}/>
-            </Route>
-        </Routes>
-    );
-};
+  return(
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<SideBar />}>
+          <Route path="/shift" element={<Shift />}/>
+          <Route path="/schedule" element={<Schedule />}/>
+          <Route path="/calendar" element={<Calendar />}/>
+          <Route path="/kinmu" element={<KimuKeitai />}/>
+          <Route path="/sheet" element={<Sheet />}/>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
